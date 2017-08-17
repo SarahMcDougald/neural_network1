900c507768e8e3265a6aa6139bb25266f443b2cc
Sarah McDougald <sarahmcdougald2020@u.northwestern.edu>
2017-08-17 01:12:17 -0700

    minor changes

--- src/com/somename/NN_Layer.java
+++ src/com/somename/NN_Layer.java
@@ -1,9 +1,20 @@
 package com.somename;
 
+import java.util.ArrayList;
+
 /**
  * Created by Sarah on 8/12/2017.
  */
 public class NN_Layer
 {
+
+    public ArrayList<Neural_Node> myNodes;
+
+    public NN_Layer()
+    {
+        myNodes = new ArrayList<Neural_Node>();
+
+    }
+
 
 }
