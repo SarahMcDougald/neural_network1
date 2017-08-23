f87c81e55aa08d93b2e1d6026fa64d27ba54432e
Sarah McDougald <sarahmcdougald2020@u.northwestern.edu>
2017-08-22 20:10:23 -0700

    added new class Network; added sigmoid() and derivSigmoid() to ActivationFunction class

--- src/com/somename/ActivationFunction_Listing.java
+++ src/com/somename/ActivationFunction_Listing.java
@@ -1,5 +1,7 @@
 package com.somename;
 
+import java.lang.Math;
+
 /**
  * Created by Sarah on 8/12/2017.
  */
@@ -30,7 +32,7 @@ public class ActivationFunction_Listing
      * "Leaky" RELU function, in which the derivative is never 0, but a negligible slope when x < 0.
      * 'x' being the sum of the input values of a particular neural node.
      *
-     * @param double inputSum the sum of the input values of a particular neural node
+     * @param inputSum the sum of the input values of a particular neural node
      * @return double output -the output produced by a particular neural node
      */
     public double leakyRELU(double inputSum)
@@ -50,5 +52,37 @@ public class ActivationFunction_Listing
     }
 
 
+    /**
+     *
+     * @param inputSum the sum of all the weights * inputs, for one node.
+     * @return
+     */
+    public double sigmoid(double inputSum)
+    {
+        double sig = 0.0;
+
+        sig = 1.0 / (1 + Math.exp(-inputSum) ); // where exp() is e^(x).
+
+        return sig;
+
+    }
+
+    /**
+     *
+     *
+     * @param inputSum the same value as is put into sigmoid(), i.e. the sum of all the weights and inputs of one node.
+     * @return
+     */
+    public double derivSigmoid(double inputSum)
+    {
+        double derivSig = inputSum*(1.0 - inputSum);
+
+        //double check tutorial, logic of equation: https://databoys.github.io/Feedforward/
+        // (my derivative-of-sigmoid function is slightly different, I substituted)
+
+
+        return derivSig;
+
+    }
 
 }
---
+++ src/com/somename/Network.java
@@ -0,0 +1,22 @@
+package com.somename;
+
+/**
+ * Created by Sarah on 8/17/2017.
+ */
+public class Network
+{
+
+    //Parts of a neural network:
+
+
+    public Network(int numLayers)
+    {
+
+
+    }
+
+
+
+
+
+}
